56416c46efe0a120e840b8d10e9af8beaef56f73
rtux754 <rozzaqm05@gmail.com>
2026-06-01 13:42:23 +0800

    Commit for CMake folder

---
+++ build/CMakeFiles/Finance_Management.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Finance_Management.dir/link.d"
+  "CMakeFiles/Finance_Management.dir/main.cpp.o"
+  "CMakeFiles/Finance_Management.dir/main.cpp.o.d"
+  "Finance_Management"
+  "Finance_Management.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Finance_Management.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
